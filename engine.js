2afdf616d42259c5d5b75b55fcc8ffa68a479705
Aleksi Hiltunen <Aleksi Hiltunen>
2017-03-31 11:12:52 +0300

    improvement, added postMessages and pause

--- engine.js
+++ engine.js
@@ -1,28 +1,39 @@
-
+var canvas;
+var ctx;
+var status=false;
+var current_score;
+var block = 20;
+var direction;
+var snake_lenght;
+var snake_position;
+var food;
+var refresh_rate = 100;
+var pause;
+var keys = {
+	65	: 'left',
+    37	: 'left',
+	87 	: 'up',
+    38  : 'up',
+	68	: 'right',
+    39  : 'right',
+	83	: 'down',
+    40  : 'down'
+};
+var pause_keys = {
+	80	: 'pause'
+};
 
 function start_game(){
 
-	var canvas = document.getElementById('gameCanvas');
-    var ctx = canvas.getContext('2d');
-    var status=false;
-    var score = 0;
-	var block = 20;
-	var direction = "down";
-	var snake_lenght = 4;
-	var snake_position = [[1,1], [2,1], [3,1], [4,1]];
-	var food = [-1,-1];
-	var refresh_rate = 100;
-	var score = 0;
-	var keys = {
-		65	: 'left',
-        37	: 'left',
-		87 	: 'up',
-        38  : 'up',
-		68	: 'right',
-        39  : 'right',
-		83	: 'down',
-        40  : 'down'
-    };
+	canvas = document.getElementById('gameCanvas');
+    ctx = canvas.getContext('2d');
+    status=false;
+    current_score = 0;
+	direction = "down";
+	snake_lenght = 4;
+	snake_position = [[1,1], [2,1], [3,1], [4,1]];
+	food = [-1,-1];
+	pause = 0;
 	var direction_changed = false;
 
 	drawSnake();
@@ -30,18 +41,29 @@ function start_game(){
 	loop();
 
 	function loop(){
-		ctx.clearRect(0,0,canvas.width,canvas.height);
-		getNewPosition();
-		drawFood();
-		console.log(snake_position[snake_lenght-1][1]);
-		if(checkCollision() == 1){
-			console.log("End Game");
-			drawSnake();
-			return;
+		var i = 0
+		if(pause == 1){ 
+			i = 1; 
+			ctx.font="108px Arial";
+			ctx.fillText("PAUSED", 200, 350);
 		}
-		drawSnake();
-		direction_changed = false;
-		document.getElementById("score").innerHTML = score;
+		while(i < 1){
+			ctx.clearRect(0,0,canvas.width,canvas.height);
+			getNewPosition();
+			drawFood();
+			console.log(snake_position[snake_lenght-1][1]);
+			if(checkCollision() == 1){
+				console.log("End Game");
+				drawSnake();
+				ctx.font="108px Arial";
+				ctx.fillText("YOU DIED! :'(", 100, 350);
+				return;
+			}
+			drawSnake();
+			direction_changed = false;
+			document.getElementById("score").innerHTML = current_score;
+		++i;
+		}
 		status = setTimeout(function() { loop(); },refresh_rate);
 	}
 
@@ -59,28 +81,28 @@ function start_game(){
 			snake_lenght += 1;
 			snake_position.push(food);
 			spawnFood();
-			score += 1;
+			current_score += 1;
 			return;
 		}
 		else if(direction == "left" && snake_position[snake_lenght-1][0]-1 == food[0] && snake_position[snake_lenght-1][1] == food[1]){
 			snake_lenght += 1;
 			snake_position.push(food);
 			spawnFood();
-			score += 1;
+			current_score += 1;
 			return;
 		}
 		else if(direction == "up" && snake_position[snake_lenght-1][0] == food[0] && snake_position[snake_lenght-1][1]-1 == food[1]){
 			snake_lenght += 1;
 			snake_position.push(food);
 			spawnFood();
-			score += 1;
+			current_score += 1;
 			return;
 		}
 		else if(direction == "down" && snake_position[snake_lenght-1][0] == food[0] && snake_position[snake_lenght-1][1]+1 == food[1]){
 			snake_lenght += 1;
 			snake_position.push(food);
 			spawnFood();
-			score += 1;
+			current_score += 1;
 			return;
 		}
 		var i = 0;
@@ -104,16 +126,27 @@ function start_game(){
 	}
 
 	window.onkeydown = function(event){
+		pause_key_pressed = pause_keys[event.keyCode];
+		if(pause_key_pressed){
+			if(pause == 1){pause = 0;}
+			else if(pause == 0){
+				pause = 1;
+				
+			}
+			
+		}
 		if(direction_changed == true){
 			return;
 		}
 		direction_changed = true;
 		new_direction = keys[event.keyCode];
+		
 		if(new_direction){
 			changeDirection(direction);
 			event.preventDefault();
 		}
-    };
+		
+	};
 
 	function changeDirection() {
 		if(new_direction == "up" && direction != "down"){
@@ -176,4 +209,60 @@ function start_game(){
 		ctx.fillRect(food[0]*block,food[1]*block,block,block);
 		return;
 	}
+	
+	
+	
+	function load_request(){
+		console.log("load_request attempt");
+		
+	return;
+	}
+}
+
+function submit_score(){
+	var message_score = {
+		messageType: "SCORE",
+		score: current_score //integer
+	}
+	window.parent.postMessage("submit", message_score);
+	return;
+}
+
+function load_request(){
+	var message_load_request = {
+		messageType: "LOAD_REQUEST"
+	}
+	this.addEventListener("message", receiveMessage, false);
+	window.parent.postMessage("load request", message_load_request);
+	if(receiveMessage.messageType == "ERROR"){
+		console.log("No game to be loaded");
+	}
+	else if(receiveMessage.messageType == "LOAD"){
+		//TODO 
+		//LOAD GAME
+		
+		//After load is done, send setting message to service	
+		var message_setting = {
+			messageType: "SETTING",
+			options: {
+				"width" : "800",
+				"height" : "600"
+			}
+		}
+		window.parent.postMessage("settings", message_setting);
+	}
+}
+
+function save(){
+	var message_save = {
+		messageType: "SAVE",
+		gameState: {
+			snake_len: snake_lenght,
+			snake_pos: snake_position,
+			score: current_score,
+			food_place: food,
+			dir: direction
+		}
+	}
+	window.parent.postMessage("save game", message_save);
 }
